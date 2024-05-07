d3623a2f9c3edc81739e152f93b7937f1eb033a1
ZahonyiBenjamin <zahonyi.benjamin.1055@dszcbaross.edu.hu>
2024-05-07 10:26:07 +0200

    separate js files

--- latvanyossagok.js
+++ latvanyossagok.js
@@ -1,72 +1,9 @@
-latvanyossagok = [
-  {
-    "nev": "Niagara-vízesés",
-    "tipus": "Vízesés",
-    "hely": "Ontario",
-    "leiras": "A Niagara-vízesés a világ egyik legismertebb természeti csodája. Az Egyesült Államok és Kanada határán fekszik, és lenyűgöző látványt nyújt.",
-    "kep": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Niagara_Falls_2009.jpg/800px-Niagara_Falls_2009.jpg"
-  },
-  {
-    "nev": "Rocky-hegység",
-    "tipus": "Hegység",
-    "hely": "Alberta, Brit Kolumbia",
-    "leiras": "A Rocky-hegység lenyűgöző hegységekkel, gleccserekkel és turistaösvényekkel rendelkezik. Számos outdoor tevékenységet kínál, például túrázást, síelést és hegymászást.",
-    "kep": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Moraine_Lake_17092005.jpg/800px-Moraine_Lake_17092005.jpg"
-  },
-  {
-    "nev": "Banff Nemzeti Park",
-    "tipus": "Nemzeti park",
-    "hely": "Alberta",
-    "leiras": "A Banff Nemzeti Park az egyik legrégebbi és legszebb nemzeti park Kanadában. Lenyűgöző hegyek, türkizkék tavak és gazdag vadvilág jellemzi.",
-    "kep": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Banff-Nationalpark_-_Moraine_Lake.jpg/800px-Banff-Nationalpark_-_Moraine_Lake.jpg"
-  },
-  {
-    "nev": "Jasper Nemzeti Park",
-    "tipus": "Nemzeti park",
-    "hely": "Alberta",
-    "leiras": "A Jasper Nemzeti Park az UNESCO Világörökség része, és a Rocky-hegységben található. Gyönyörű tavak, gleccserek és vadon élő állatok otthona.",
-    "kep": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Mount_Fryatt_-_Jasper_National_Park_-_Alberta_-_Canada_-_02.jpg/800px-Mount_Fryatt_-_Jasper_National_Park_-_Alberta_-_Canada_-_02.jpg"
-  },
-  {
-    "nev": "Yoho Nemzeti Park",
-    "tipus": "Nemzeti park",
-    "hely": "Brit Kolumbia",
-    "leiras": "A Yoho Nemzeti Park lenyűgöző hegyi tájakkal, vízesésekkel és barlangokkal rendelkezik. A Burgess Shale egyedülálló őskori fosszíliákat tartalmaz.",
-    "kep": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Yoho_National_Park%2C_British_Columbia%2C_Canada_%284148400293%29.jpg/800px-Yoho_National_Park%2C_British_Columbia%2C_Canada_%284148400293%29.jpg"
-  },
-  {
-    "nev": "Whistler",
-    "tipus": "Síparadicsom",
-    "hely": "Brit Kolumbia",
-    "leiras": "Whistler Kanada egyik legnépszerűbb síparadicsoma. Nyáron is sok outdoor tevékenységet kínál, mint például túrázást és mountain bike-ozást.",
-    "kep": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Whistler_%2816726280317%29.jpg/800px-Whistler_%2816726280317%29.jpg"
-  },
-  {
-    "nev": "Gros Morne Nemzeti Park",
-    "tipus": "Nemzeti park",
-    "hely": "Új-Fundland",
-    "leiras": "A Gros Morne Nemzeti Park lenyűgöző partvidékkel, fjordokkal és hegycsúcsokkal rendelkezik. Az UNESCO Világörökség része.",
-    "kep": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Gros_Morne_Mountain.JPG/800px-Gros_Morne_Mountain.JPG"
-  },
-  {
-    "nev": "Nahanni Nemzeti Park",
-    "tipus": "Nemzeti park",
-    "hely": "Északnyugat-terület",
-    "leiras": "A Nahanni Nemzeti Park a világ egyik legszebb és legvadregényesebb területe. Vízesések, kanyonok és vadon élő állatok jellemzik.",
-    "kep": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Nahanni_national_park_%28Canada%29.jpg/800px-Nahanni_national_park_%28Canada%29.jpg"
-  },
-  {
-    "nev": "CN Tornya",
-    "tipus": "Világítótorony",
-    "hely": "Toronto",
-    "leiras": "A CN Tornya Toronto egyik legismertebb jelképe. A kilátóteraszról lenyűgöző kilátás nyílik a városra és a tóra.",
-    "kep": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/CN_Tower_at_sunset.jpg/800px-CN_Tower_at_sunset.jpg"
-  },
-  {
-    "nev": "Churchill",
-    "tipus": "Állatmegfigyelés",
-    "hely": "Manitoba",
-    "leiras": "Churchill Manitoba északi részén található és a jeges medvék megfigyelésének egyik legjobb helyszíne a világon. Ezenkívül itt lehetőség van a sarki rókák és a tengeri emlősök megfigyelésére is.",
-    "kep": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Churchill_polar_bears_%2825455784409%29.jpg/800px-Churchill_polar_bears_%2825455784409%29.jpg"
-  }
-]
+let latv = document.getElementById("latvanyossagok")
+console.log(latvanyossagok)
+for (const item of latvanyossagok)
+{
+    latv.innerHTML += `
+    <button>${item.nev}</button>
+    `
+    console.log(item.nev)
+}
---
+++ varosok_adatok.js
@@ -0,0 +1,65 @@
+varosok = [
+    {
+      "nev": "Toronto",
+      "terulet": "630.2 km²",
+      "nepesseg": "2,731,571 (2016)",
+      "leiras": "Toronto Kanada legnagyobb városa és Ontario tartomány fővárosa. Gazdaságilag és kulturálisan is az ország egyik vezető városa.",
+      "kep": "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/147000/147900-Toronto.jpg"
+    },
+    {
+      "nev": "Montreal",
+      "terulet": "431.5 km²",
+      "nepesseg": "1,704,694 (2016)",
+      "leiras": "Montreal Quebec tartomány legnagyobb városa és egyben gazdasági központja. Az egyik legnagyobb francia nyelvű város a világon.",
+      "kep": "https://travel.destinationcanada.com/_next/image?url=https%3A%2F%2Fadmin.destinationcanada.com%2Fsites%2Fdefault%2Ffiles%2F2023-06%2FQC-Montreal-Skyline_hero.jpg&w=1920&q=75"
+    },
+    {
+      "nev": "Vancouver",
+      "terulet": "115 km²",
+      "nepesseg": "631,486 (2016)",
+      "leiras": "Vancouver a Brit Columbia tartomány fővárosa és Kanada egyik legnagyobb és legjelentősebb kikötője.",
+      "kep": "https://upload.wikimedia.org/wikipedia/commons/5/57/Concord_Pacific_Master_Plan_Area.jpg"
+    },
+    {
+      "nev": "Ottawa",
+      "terulet": "2,778 km²",
+      "nepesseg": "934,243 (2016)",
+      "leiras": "Ottawa Kanada fővárosa, Ontario tartományban, az Ottawa és a Rideau folyó torkolata mellett helyezkedik el. Gazdasági, politikai és kulturális jelentőséggel bír.",
+      "kep": "https://workingholidayincanada.com/wp-content/uploads/2021/03/Ottawa-min.jpg"
+    },
+    {
+      "nev": "Calgary",
+      "terulet": "825.29 km²",
+      "nepesseg": "1,096,833 (2016)",
+      "leiras": "Calgary Alberta tartomány legnagyobb városa, a kanadai Rockiesok lábánál, a Bow folyó partján fekszik. Gazdaságilag jelentős az olaj- és gázipar miatt.",
+      "kep": "https://www.worldatlas.com/upload/8b/1e/95/shutterstock-631376156.jpg"
+    },
+    {
+      "nev": "Edmonton",
+      "terulet": "684.37 km²",
+      "nepesseg": "932,546 (2016)",
+      "leiras": "Edmonton Alberta tartomány fővárosa és Kanada egyik legnagyobb városa. Gazdasági központ és fontos ipari, kereskedelmi, és kulturális központ.",
+      "kep": "https://www.worldatlas.com/upload/d5/7a/0a/shutterstock-1445647262.jpg"
+    },
+    {
+      "nev": "Québec City",
+      "terulet": "485.77 km²",
+      "nepesseg": "531,902 (2016)",
+      "leiras": "Québec City Quebec tartomány fővárosa és Kanada egyik legrégebbi városa. Gazdag történelmi és kulturális örökséggel rendelkezik.",
+      "kep": "https://travel.destinationcanada.com/_next/image?url=https%3A%2F%2Fadmin.destinationcanada.com%2Fsites%2Fdefault%2Ffiles%2F2023-06%2FQC-Chateau-Frontenac_hero.jpg&w=1920&q=75"
+    },
+    {
+      "nev": "Winnipeg",
+      "terulet": "464.08 km²",
+      "nepesseg": "705,244 (2016)",
+      "leiras": "Winnipeg Manitoba tartomány fővárosa és Kanada központjában fekszik. Fontos ipari, kereskedelmi és kulturális központ.",
+      "kep": "https://i.natgeofe.com/n/77f528cb-054d-4bdb-8a4e-15a1c16d5195/winnipeg-skyline-canada_3x2.jpg"
+    },
+    {
+      "nev": "Hamilton",
+      "terulet": "1,117 km²",
+      "nepesseg": "536,917 (2016)",
+      "leiras": "Hamilton Ontario tartományban fekszik, a tóparti régió egyik legfontosabb városa. Fontos gazdasági központ és kulturális központ.",
+      "kep": "https://cdn.britannica.com/96/115796-050-3B9D84C6/Hamilton-Ont.jpg"
+    }
+]
