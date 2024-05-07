88fb65a80cc70a7a0b8bd5c65f8310cb5f022d2a
ZahonyiBenjamin <zahonyi.benjamin.1055@dszcbaross.edu.hu>
2024-05-07 10:05:48 +0200

    first commit

---
+++ nav.js
@@ -0,0 +1,28 @@
+document.getElementById("menu").innerHTML = `
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.html">Kanada</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="varosok.html">Városok</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="latvannyosagok.html">Látványosságok</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="parkok.html">Nemzeti Parkok</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="szemelyek.html">Nevezetes Személyek</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="etelek.html">Ételek</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>`
