b4c0ea58472dafef09105e4aac6bc0d475bb3d27
ZahonyiBenjamin <zahonyi.benjamin.1055@dszcbaross.edu.hu>
2024-05-15 11:52:33 +0200

    nemzeti parkok finish

--- parkok.js
+++ parkok.js
@@ -21,7 +21,24 @@ for (const item of latvanyossagok)
         {
             document.getElementById("car_belso").innerHTML += `
             <div class="carousel-item">
-                <img src="${item.kep}" alt="${item.kep}" class="d-block w-100">
+                <img src="${item.kep}" alt="${item.kep}" class="d-block w-100 park_kepek">
+                <div class="carousel-caption">
+                <h2 class="park_szoveg">${item.nev}</h2>
+                <h4 class="park_szoveg">${item.hely}</h4>
+                <p class="park_szoveg">${item.leiras}</p>
+              </div>
+            </div>` 
+        }
+        else
+        {
+            document.getElementById("car_belso").innerHTML += `
+            <div class="carousel-item active">
+                <img src="${item.kep}" alt="${item.kep}" class="d-block w-100 park_kepek">
+                <div class="carousel-caption">
+                <h2 class="park_szoveg">${item.nev}</h2>
+                <h4 class="park_szoveg">${item.hely}</h4>
+                <p class="park_szoveg">${item.leiras}</p>
+              </div>
             </div>` 
         }
 
